8564afa8ea9a4f5ddd9f531db9f609e1d4906570
cernajs <adamcerny13@icloud.com>
2023-08-20 21:08:58 +0200

    catchup

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TwitterClone.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<Tweet> Tweets { get; set; } = new List<Tweet>();
+
+        public virtual ICollection<UserFollower> Followers { get; set; } = new List<UserFollower>();
+        public virtual ICollection<UserFollower> Following { get; set; } = new List<UserFollower>();
+    }
+}
---
+++ Models/UserFollower.cs
@@ -0,0 +1,12 @@
+namespace TwitterClone.Data;
+
+public class UserFollower
+{
+    // Follower
+    public string FollowerId { get; set; }
+    public virtual ApplicationUser Follower { get; set; }
+
+    // Following
+    public string FollowingId { get; set; }
+    public virtual ApplicationUser Following { get; set; }
+}
