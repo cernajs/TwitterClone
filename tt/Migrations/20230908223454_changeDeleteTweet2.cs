bebdaf96e469585ba8530349102570272d87f3f6
cernajs <adamcerny13@icloud.com>
2023-09-09 01:01:46 +0200

    commit stages

---
+++ tt/Migrations/20230908223454_changeDeleteTweet2.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tt.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeDeleteTweet2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Retweets_Tweets_TweetId",
+                table: "Retweets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Retweets_Tweets_TweetId",
+                table: "Retweets",
+                column: "TweetId",
+                principalTable: "Tweets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Retweets_Tweets_TweetId",
+                table: "Retweets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Retweets_Tweets_TweetId",
+                table: "Retweets",
+                column: "TweetId",
+                principalTable: "Tweets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
